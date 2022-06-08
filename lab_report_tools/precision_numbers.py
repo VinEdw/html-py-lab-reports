6d04c5d55abc6e2b5a48d278e8cad22d194a0a1b
Vincent Edwards <mister.man489@gmail.com>
2022-06-08 13:58:51 -0700

    count_sig_figs static method added

--- lab_report_tools/precision_numbers.py
+++ lab_report_tools/precision_numbers.py
@@ -1,7 +1,13 @@
+import re
+
 class PresisonNumber:
     def __init__(self, value_str: str, *, sigFigs: int, decimalPlace: int, absoluteError: float, relativeError: float):
         self.value_str = value_str
         self.value = float(value_str)
+    
+    @staticmethod
+    def count_sig_figs(value_str: str) -> int:
+        value = float(value_str)
 
 
 if __name__ == "__main__":
