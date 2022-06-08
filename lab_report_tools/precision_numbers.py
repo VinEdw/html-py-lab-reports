0f731d5d7c744a17e8f2236bdd258a875404aae7
Vincent Edwards <mister.man489@gmail.com>
2022-06-08 13:55:46 -0700

    __init__ annotations

--- lab_report_tools/precision_numbers.py
+++ lab_report_tools/precision_numbers.py
@@ -1,5 +1,5 @@
 class PresisonNumber:
-    def __init__(self, value_str, *, sigFigs, decimalPlaces, absoluteError, relativeError):
+    def __init__(self, value_str: str, *, sigFigs: int, decimalPlace: int, absoluteError: float, relativeError: float):
         self.value_str = value_str
         self.value = float(value_str)
 
