7387c97f4d5e7159bb6a3ea763783760d4a437ab
Vincent Edwards <mister.man489@gmail.com>
2022-06-08 18:35:36 -0700

    str|float|int for value

--- lab_report_tools/precision_numbers.py
+++ lab_report_tools/precision_numbers.py
@@ -1,9 +1,20 @@
 import re
 
 class PrecisionNumber:
-    def __init__(self, value_str: str, *, sig_figs: int, decimal_place: int, absolute_error: float, relative_error: float):
-        self.value_str = value_str
-        self.value = float(value_str)
+    def __init__(self, value: str|float|int, *, sig_figs: int, decimal_place: int, absolute_error: float, relative_error: float):
+        value_type = type(value)
+        if (value_type is int) or (value_type is float):
+            self._value = value
+        elif value_type is str:
+            self._value = float(value)
+        else:
+            raise TypeError("PrecisionNumber value argument must be of type str|float|int")
+        
+    @property
+    def value(self):
+        """Get the number's value."""
+        return self._value
+
     
     @staticmethod
     def count_sig_figs(value_str: str) -> int:
