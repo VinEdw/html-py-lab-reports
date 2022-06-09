08de941619d2885e2a6ab3acace30302471bb8fd
Vincent Edwards <mister.man489@gmail.com>
2022-06-08 21:06:37 -0700

    __init__ updated, but property() 's still needed

--- lab_report_tools/precision_numbers.py
+++ lab_report_tools/precision_numbers.py
@@ -1,14 +1,42 @@
 import re
 
 class PrecisionNumber:
-    def __init__(self, value: str|float|int, *, sig_figs: int, decimal_place: int, absolute_error: float, relative_error: float):
+    def __init__(self, value: str|float|int, *, sig_figs: int = None, decimal_place: int = None, absolute_error: float = None, relative_error: float = None):
         value_type = type(value)
-        if (value_type is int) or (value_type is float):
-            self._value = value
-        elif value_type is str:
-            self._value = float(value)
-        else:
+        if not ((value_type is int) or (value_type is float) or (value_type is str)):
             raise TypeError("PrecisionNumber value argument must be of type str|float|int")
+        self._value = float(value)
+
+        if sig_figs == None and decimal_place == None:
+            if value_type is not str:
+                raise ValueError("*sig_figs* and *decimal_place* cannot both be None if *value* is not a str")
+            self.sig_figs = self.count_sig_figs(value)
+        elif sig_figs != None and decimal_place == None:
+            if type(sig_figs) is not int:
+                raise TypeError("*sig_figs* argument should be an int")
+            self.sig_figs = sig_figs
+        elif sig_figs == None and decimal_place != None:
+            if type(decimal_place) is not int:
+                raise TypeError("*decimal_place* argument should be an int")
+            self.decimal_place = decimal_place
+        else:
+            raise ValueError("*sig_figs* and *decimal_place* cannot both be defined")
+
+        if absolute_error == None and relative_error == None:
+            if value_type is not str:
+                raise ValueError("*absolute_error* and *relative_error* cannot both be None if *value* is not a str")
+            self.absolute_error = self.get_absolute_error(value)
+        elif absolute_error != None and relative_error == None:
+            if type(absolute_error) is not float:
+                raise TypeError("*absolute_error* argument should be an float")
+            self.absolute_error = absolute_error
+        elif absolute_error == None and relative_error != None:
+            if type(relative_error) is not float:
+                raise TypeError("*relative_error* argument should be an float")
+            self.relative_error = relative_error
+        else:
+            raise ValueError("*absolute_error* and *relative_error* cannot both be defined")
+            
         
     @property
     def value(self):
@@ -18,7 +46,24 @@ class PrecisionNumber:
     
     @staticmethod
     def count_sig_figs(value_str: str) -> int:
+        """Count the number of significant figures (sig figs) in the input number string."""
         value = float(value_str)
+
+    # @staticmethod
+    # def get_decimal_place(value_str: str) -> int:
+    #     """Get the place value of the lowest decimal place in the input number string.
+    #     The returned value is the power 10 would need to be raised to in order to be in the proper place value."""
+    #     value = float(value_str)
+    
+    @staticmethod
+    def get_absolute_error(value_str: str) -> int:
+        """Get the absolute error of the input number string."""
+        value = float(value_str)
+
+    # @staticmethod
+    # def get_relative_error(value_str: str) -> int:
+    #     """Get the relative error of the input number string."""
+    #     value = float(value_str)
 
 if __name__ == "__main__":
     import unittest
