db49a4f55eac48aafc21abcfb3101a80053401d8
Vincent Edwards <mister.man489@gmail.com>
2022-06-08 18:19:43 -0700

    one test added

--- lab_report_tools/precision_numbers.py
+++ lab_report_tools/precision_numbers.py
@@ -1,6 +1,6 @@
 import re
 
-class PresisonNumber:
+class PrecisionNumber:
     def __init__(self, value_str: str, *, sig_figs: int, decimal_place: int, absolute_error: float, relative_error: float):
         self.value_str = value_str
         self.value = float(value_str)
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     class TestSigFigCounter(unittest.TestCase):
         
         def no_decmial_point(self):
-            pass
+            self.assertEqual(PrecisionNumber.count_sig_figs("120"), 3)
 
     unittest.main()
