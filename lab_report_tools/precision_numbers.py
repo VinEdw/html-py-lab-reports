ae519b61f159cb00cbb5c6c6e38865f402f691b5
Vincent Edwards <mister.man489@gmail.com>
2022-06-08 21:39:44 -0700

    __init__ update

--- lab_report_tools/precision_numbers.py
+++ lab_report_tools/precision_numbers.py
@@ -1,16 +1,14 @@
 import re
 
 class PrecisionNumber:
-    def __init__(self, value: str|float|int, *, sig_figs: int = None, decimal_place: int = None, absolute_error: float = None, relative_error: float = None):
-        value_type = type(value)
-        if not ((value_type is int) or (value_type is float) or (value_type is str)):
-            raise TypeError("PrecisionNumber value argument must be of type str|float|int")
-        self._value = float(value)
+    def __init__(self, value_str: str|float|int, *, sig_figs: int = None, decimal_place: int = None, absolute_error: float|int = None, relative_error: float|int = None):
+        value_type = type(value_str)
+        if value_type not in [str, float, int]:
+            raise TypeError("PrecisionNumber *value_str* argument must be of type str|float|int")
+        self._value = float(value_str)
 
         if sig_figs == None and decimal_place == None:
-            if value_type is not str:
-                raise ValueError("*sig_figs* and *decimal_place* cannot both be None if *value* is not a str")
-            self.sig_figs = self.count_sig_figs(value)
+            self.sig_figs = self.count_sig_figs(str(value_str))
         elif sig_figs != None and decimal_place == None:
             if type(sig_figs) is not int:
                 raise TypeError("*sig_figs* argument should be an int")
@@ -23,15 +21,13 @@ class PrecisionNumber:
             raise ValueError("*sig_figs* and *decimal_place* cannot both be defined")
 
         if absolute_error == None and relative_error == None:
-            if value_type is not str:
-                raise ValueError("*absolute_error* and *relative_error* cannot both be None if *value* is not a str")
-            self.absolute_error = self.get_absolute_error(value)
+            self.absolute_error = self.get_absolute_error(str(value_str))
         elif absolute_error != None and relative_error == None:
-            if type(absolute_error) is not float:
+            if type(absolute_error) not in [float, int]:
                 raise TypeError("*absolute_error* argument should be a float")
             self.absolute_error = absolute_error
         elif absolute_error == None and relative_error != None:
-            if type(relative_error) is not float:
+            if type(relative_error) not in [float, int]:
                 raise TypeError("*relative_error* argument should be a float")
             self.relative_error = relative_error
         else:
@@ -39,31 +35,31 @@ class PrecisionNumber:
             
         
     @property
-    def value(self):
-        """Get the number's value."""
+    def value_str(self):
+        """Get the number's value_str."""
         return self._value
 
     
     @staticmethod
     def count_sig_figs(value_str: str) -> int:
         """Count the number of significant figures (sig figs) in the input number string."""
-        value = float(value_str)
+        value_str = float(value_str)
 
     # @staticmethod
     # def get_decimal_place(value_str: str) -> int:
-    #     """Get the place value of the lowest decimal place in the input number string.
-    #     The returned value is the power 10 would need to be raised to in order to be in the proper place value."""
-    #     value = float(value_str)
+    #     """Get the place value_str of the lowest decimal place in the input number string.
+    #     The returned value_str is the power 10 would need to be raised to in order to be in the proper place value_str."""
+    #     value_str = float(value_str)
     
     @staticmethod
     def get_absolute_error(value_str: str) -> int:
         """Get the absolute error of the input number string."""
-        value = float(value_str)
+        value_str = float(value_str)
 
     # @staticmethod
     # def get_relative_error(value_str: str) -> int:
     #     """Get the relative error of the input number string."""
-    #     value = float(value_str)
+    #     value_str = float(value_str)
 
 if __name__ == "__main__":
     import unittest
