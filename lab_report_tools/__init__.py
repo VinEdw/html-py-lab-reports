a01a06938765b97d418e703b67f70956cdc4998f
Vincent Edwards <mister.man489@gmail.com>
2022-06-16 17:57:11 -0700

    file_editor added to __init__.py

--- lab_report_tools/__init__.py
+++ lab_report_tools/__init__.py
@@ -1,4 +1,5 @@
 from .precision_numbers import PrecisionNumber
 from .data_tables import DataTable
+from . import file_editor
 
-__all__ = ["PrecisionNumber", "DataTable"]
+__all__ = ["PrecisionNumber", "DataTable", "file_editor"]
