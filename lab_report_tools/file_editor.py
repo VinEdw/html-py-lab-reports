404f49e1431fca1f18540628aaaf34959222c7dc
Vincent Edwards <mister.man489@gmail.com>
2022-06-16 17:56:48 -0700

    file_editor.py created

---
+++ lab_report_tools/file_editor.py
@@ -0,0 +1,18 @@
+import re
+
+def write_between_tags(text: str, id: str, tag: str, file_name: str):
+    """In the identified text file, write the input text between tags that look like the following.
+    <*tag* id="*id*">
+    ...
+    </*tag*>
+    Note that this must match exactly, otherwise it will not work.
+    The goal is to make it only match a very limited portion of the document.
+    """
+    with open(file_name, "r") as f:
+        file_text = f.read()
+    text = "\n\n" + text + "\n\n"
+    pattern = fr'(?<=<{tag} id="{id}">).+?(?=</{tag}>)'
+    substitution, n = re.subn(pattern, text, file_text, 0, re.DOTALL)
+    if n != 0:
+        with open(file_name, "w", encoding="utf-8") as f:
+            f.write(substitution)
