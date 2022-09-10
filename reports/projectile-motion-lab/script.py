44213353aab6704bcfe40a31902a86e0276a7ef2
Vincent Edwards <mister.man489@gmail.com>
2022-09-09 19:19:53 -0700

    angled distance table created

--- reports/projectile-motion-lab/script.py
+++ reports/projectile-motion-lab/script.py
@@ -1,6 +1,6 @@
 from lab_report_tools import *
 
-# Get the tables into the report
+# Get the logger pro tables into the report
 
 slow_ball_table = DataTable.from_csv("raw-data/slow-ball-data.csv")
 fast_ball_table = DataTable.from_csv("raw-data/fast-ball-data.csv")
@@ -30,7 +30,7 @@ file_editor.write_between_markers(slow_ball_table.get_html(
 file_editor.write_between_markers(fast_ball_table.get_html(
     caption="Run 2 Logger Pro Video Data"), "fast-ball-table", "projectile-motion-lab-report.html")
 
-# Get the graphs into the report
+# Get logger pro graphs into the report
 
 run_list = ["slow", "fast"]
 graph_order = ["x-t", "y-t", "vx-t", "vy-t", "y-x"]
@@ -51,3 +51,12 @@ for i, run in enumerate(run_list):
         graph_caption = f"Run {i + 1}—{symbol_name_map[y_ax]} vs {symbol_name_map[x_ax]}"
         text += figure_template.format(caption=graph_caption, src=file_name) + "\n"
     file_editor.write_between_markers(text, f"{run}-ball-graphs", "projectile-motion-lab-report.html")
+
+# Get distance along angled board data into the report
+
+angled_distance_data = DataTable.from_csv("raw-data/distance-along-angled-board.csv")
+angled_distance_data.create_column([i for i in range(1, angled_distance_data.row_count + 1)], "Trial", index=0, label="Trial", use_dict=False)
+angled_distance_data["d (cm)"] = [PrecisionNumber(val, decimal_place=-2) for val in angled_distance_data["d (cm)"]]
+print(angled_distance_data)
+angled_distance_avg = sum(angled_distance_data["d (cm)"]) / len(angled_distance_data["d (cm)"])
+print(angled_distance_avg)
