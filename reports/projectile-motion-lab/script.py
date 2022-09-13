b1c51ebc1699e36c098ea6e6abc94cc893eba75b
Vincent Edwards <mister.man489@gmail.com>
2022-09-12 18:50:21 -0700

    graph code removed since figures need descriptions

--- reports/projectile-motion-lab/script.py
+++ reports/projectile-motion-lab/script.py
@@ -30,28 +30,6 @@ file_editor.write_between_markers(slow_ball_table.get_html(
 file_editor.write_between_markers(fast_ball_table.get_html(
     caption="Run 2 Logger Pro Video Data"), "fast-ball-table", "projectile-motion-lab-report.html")
 
-# Get logger pro graphs into the report
-
-run_list = ["slow", "fast"]
-graph_order = ["x-t", "y-t", "vx-t", "vy-t", "y-x"]
-figure_template = '<figure>\n  <img src="{src}">\n  <figcaption>{caption}</figcaption>\n</figure>'
-symbol_name_map = {
-    "t": "Time",
-    "x": "Horizontal Position",
-    "y": "Vertical Position",
-    "vx": "Horizontal Velocity",
-    "vy": "Vertical Velocity",
-}
-
-for i, run in enumerate(run_list):
-    text = ""
-    for graph in graph_order:
-        file_name = f"media/{run}-{graph}.png"
-        y_ax, x_ax = graph.split("-")
-        graph_caption = f"Run {i + 1}—{symbol_name_map[y_ax]} vs {symbol_name_map[x_ax]}"
-        text += figure_template.format(caption=graph_caption, src=file_name) + "\n"
-    file_editor.write_between_markers(text, f"{run}-ball-graphs", "projectile-motion-lab-report.html")
-
 # Get distance along angled board data into the report
 
 angled_distance_data = DataTable.from_csv("raw-data/distance-along-angled-board.csv")
