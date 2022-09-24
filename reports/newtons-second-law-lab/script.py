bf5ffa5d4c33aad946d64872e52448e27b1a76fc
Vincent Edwards <mister.man489@gmail.com>
2022-09-23 18:07:08 -0700

    hanging mass acceleration graph made

--- reports/newtons-second-law-lab/script.py
+++ reports/newtons-second-law-lab/script.py
@@ -1,4 +1,5 @@
 from lab_report_tools import *
+import matplotlib.pyplot as plt
 
 file_name = "newtons-second-law-lab-report.html"
 
@@ -32,3 +33,19 @@ file_editor.write_between_markers(m_a_tb.get_html(
     caption="Hanging Mass & Acceleration Data"), "hanging-mass-acceleration-table", file_name)
 
 print(m_a_tb)
+
+# Plot average acceleration versus hanging mass
+
+m_a_fig, m_a_ax = plt.subplots(nrows=1, ncols=1, figsize=(8, 4))
+m_a_ax.plot(m_a_tb["m_h"], m_a_tb["a_avg"], "bo")
+m_a_ax.set_title("Average Acceleration vs Hanging Mass")
+m_a_ax.grid(True, which="minor") # default linewidth is 0.8 (I think)
+m_a_ax.grid(True, which="major", linewidth=1.5)
+# Customize the x-axis and y-axis
+graphing.style_axis(m_a_ax, "x", 0, 30, label=m_a_tb.labels["m_h"], major_tick_spacing=5)
+graphing.style_axis(m_a_ax, "y", 0, 0.8, label=m_a_tb.labels["a_avg"], major_tick_spacing=0.2, major_tick_formatter="{x:.4f}")
+# Find and plot the line of best fit
+y_int, slope, R_squared = graphing.plot_best_fit_line(m_a_ax, m_a_tb["m_h"], m_a_tb["a_avg"], "r")
+# Save the figure as a png
+m_a_fig.tight_layout()
+m_a_fig.savefig("media/hanging-mass-acceleration-graph.png")
